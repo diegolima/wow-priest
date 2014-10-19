c533f41446853bf618c0a1a66247d2770f6e5dee
diegolima <diego@diegolima.org>
2014-10-19 13:39:10 -0200

    Added base files

---
+++ SavedVariables/Bagnon.lua
@@ -0,0 +1,16 @@
+
+BagnonFrameSettings = {
+	["version"] = "6.0.9",
+	["frames"] = {
+		["inventory"] = {
+			["itemFrameColumns"] = 10,
+			["y"] = 0,
+			["x"] = -154.504028320313,
+		},
+		["guildbank"] = {
+			["point"] = "BOTTOMLEFT",
+			["y"] = 158.590911865234,
+			["x"] = 327.606048583984,
+		},
+	},
+}
